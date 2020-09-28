361279a3d03578ba2e4d60808a717c48acd4ed8b
Schroeder <Will_Schroeder1@baylor.edu>
2020-09-28 16:04:04 -0500

    9_1

--- US_fires_9_1.py
+++ US_fires_9_1.py
@@ -5,7 +5,36 @@ bseptoutfile = open('bsept_fire_data.json', 'w')
 bseptdata = json.load(bseptinfile)
 json.dump(bseptdata, bseptoutfile, indent = 4)
 
-blons, blats, brightness = [], [], []
+blons, blats, bbrightness = [], [], []
 
 for x in bseptdata:
     lon = x['longitude']
+    lat = x['latitude']
+    bright = x['brightness']
+    blons.append(lon)
+    blats.append(lat)
+    if bright > 450:
+        bbrightness.append(bright)
+
+
+from plotly.graph_objs import Scattergeo, Layout
+from plotly import offline
+
+data = [{
+    'type' : 'scattergeo',
+    'lon' : blons,
+    'lat' : blats,
+    'maker' : {
+        'size' : [.05 * bright for bright in bbrightness],
+        'color' : bbrightness,
+        'colorscale' : 'Viridis',
+        'reversescale' : True,
+        'colorbar' : {'title' : 'Brightness'}
+    },
+}]
+
+my_layout = Layout(title = 'Fires')
+
+fig = {'data' : data, 'layout' : my_layout}
+
+offline.plot(fig, filename = 'bed_sept_fires.html')
